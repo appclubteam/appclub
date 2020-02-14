041b4aa345a3ed4c06b2df95b44c89de430c5e09
huaiyukhaw <huaiyukhaw@gmail.com>
2020-02-14 17:26:20 +0800

    fix navbar buttons

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,12 +1,11 @@
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
+    <div class="navbar-brand is-flex">
       <nuxt-link to="/" class="navbar-item">
         <img src="../assets/logo.jpg" height="100%" alt="App Club logo" />
       </nuxt-link>
     </div>
-
-    <div id="topnavbar" class="navbar-menu">
+    <div class="navbar-menu">
       <div class="navbar-end">
         <nuxt-link to="/team" class="navbar-item">The Team</nuxt-link>
         <nuxt-link to="/join" class="navbar-item">
@@ -18,3 +17,39 @@
     </div>
   </nav>
 </template>
+
+<style>
+.navbar-item {
+  display: flex;
+}
+
+.navbar,
+.navbar-menu,
+.navbar-start,
+.navbar-end {
+  align-items: stretch;
+  display: flex;
+  padding: 0;
+}
+
+.navbar-menu {
+  flex-grow: 1;
+  flex-shrink: 0;
+  box-shadow: none;
+}
+.navbar-start {
+  justify-content: flex-start;
+  margin-right: auto;
+}
+.navbar-end {
+  justify-content: flex-end;
+  margin-left: auto;
+}
+.navbar .container {
+  display: flex;
+}
+.navbar-item {
+  display: flex;
+  align-items: center;
+}
+</style>

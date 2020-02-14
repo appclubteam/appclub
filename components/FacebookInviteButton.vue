6324751a1116073a823350b55eae603858387a8f
huaiyukhaw <huaiyukhaw@gmail.com>
2020-02-14 19:55:48 +0800

    add facebook button

---
+++ components/FacebookInviteButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <a
+    class="button is-link"
+    href="https://www.facebook.com/plugins/group/join/popup/?group_id=151374301571964&amp;source=email_campaign_plugin"
+    target="_blank"
+    rel="noopener"
+  >Join Facebook Group</a>
+</template>
--- pages/join/index.vue
+++ pages/join/index.vue
@@ -1,19 +1,24 @@
 <template>
 <body>
   <section class="section">
-    <div class="container">
-      <SignupForm />
+    <div class="container has-text-centered">
+      <h1 class="title">We aims to help students who build apps, or want to.</h1>
+      <h2 class="subtitle is-5">It's not what we are but we're going to be that matters.</h2>
+      <FacebookInviteButton />
     </div>
   </section>
+  <SignupForm />
 </body>
 </template>
 
 <script>
 import SignupForm from '~/components/SignupForm.vue'
+import FacebookInviteButton from '~/components/FacebookInviteButton.vue'
 
 export default {
   components: {
-    SignupForm
+    SignupForm,
+    FacebookInviteButton
   }
 }
 </script>

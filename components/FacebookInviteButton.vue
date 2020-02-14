4b1c779cf4783ba6b4a2c0227bf230563f4b606c
huaiyukhaw <huaiyukhaw@gmail.com>
2020-02-14 20:07:01 +0800

    add facebook button

--- components/FacebookInviteButton.vue
+++ components/FacebookInviteButton.vue
@@ -1,7 +1,7 @@
 <template>
   <a
     class="button is-link"
-    href="https://www.facebook.com/plugins/group/join/popup/?group_id=151374301571964"
+    href="https://www.facebook.com/plugins/group/join/popup/?group_id=408755619178957"
     target="_blank"
     rel="noopener"
   >Join Facebook Group</a>

717db153fe57f1349622f2f5b628d06f3a0153d1
huaiyukhaw <huaiyukhaw@gmail.com>
2020-02-18 17:27:00 +0800

    edit og tag image

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,7 +30,7 @@ export default {
         {
           hid: 'og:image',
           property: 'og:image',
-          content: '../assets/groupphoto.jpg'
+          content: '/assets/groupphoto.jpg'
         }
       ]
     }
